0d965241bebe58604eeb450ce8848c81d8f1c142
Aleksandr Kudakov <al.kudakov>
2020-08-07 13:12:04 +0300

    ghbf

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -26,11 +26,11 @@ public  class Util {
                 Configuration configuration = new Configuration();
 
                 Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "org.postgresql.Driver");
-                settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/postgres?useSSL=false");
-                settings.put(Environment.USER, "postgres");
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/my_db_test?serverTimezone=Europe/Moscow&useSSL=false");
+                settings.put(Environment.USER, "root");
                 settings.put(Environment.PASS, "Sentyabr@#11");
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "tread");
                 settings.put(Environment.HBM2DDL_AUTO, "create-drop");
